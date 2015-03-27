f3ca32480c8d31c085effe09a02702b9e4ae9b5d
Jordan Dashel <jordan.dashel@gmail.com>
2015-03-27 14:03:15 -0700

    final initial commit

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using MvcMusicStore.Models;
 
 namespace MvcMusicStore.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        //
+        // GET: /Home/
+        MusicStoreEntities storeDB = new MusicStoreEntities();
         public ActionResult Index()
         {
-            return View();
+            // Get most popular albums
+            var albums = GetTopSellingAlbums(5);
+
+            return View(albums);
+        }
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
         }
     }
 }
